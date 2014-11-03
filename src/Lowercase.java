72a1d5c267903ebc07b98b01d15decce7d39638c
Christian Rubiales <christianrubiales@yahoo.com>
2014-11-03 21:04:39 +0800

    Lowercase

---
+++ src/Lowercase.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+public class Lowercase {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader(args[0]));
+		
+		String line;
+		while ((line = br.readLine()) != null) {
+			System.out.println(line.toLowerCase());
+		}
+	}
+
+}
