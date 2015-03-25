3c3e08fe3e9842867966e123a0d01573091f686e
Christian Rubiales <christianrubiales@yahoo.com>
2015-03-26 00:32:13 +0800

    Locks

--- moderate/Locks.java
+++ moderate/Locks.java
@@ -1,6 +1,5 @@
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.Scanner;
 
 
@@ -9,26 +8,27 @@ public class Locks {
 	public static void main(String[] a) throws IOException {
 		Scanner s = new Scanner(new File(a[0]));
 		while (s.hasNext()) {
-			int i = s.nextInt();
-			int j = s.nextInt();
-			int c = 0;
+			int N = s.nextInt();
+			int M = s.nextInt();
 			
-			if (j % 2 == 0) { // multiples of 6 unlocked
-//				for (int x = 1; x <= i; x++) {
-//					if (x % 6 == 0) {
-//						c++;
-//					}
-//				}
-				c = i/2;
-			} else {
-				for (int x = 1; x <= i; x++) {
-					if (x % 6 != 0) { // 2-multiples and 3-multiples unlocked, but not 6-multiples
-						if (x % 2 == 0 || x % 3 == 0) {
-							c++;
-						}
-					}
+			boolean[] b = new boolean[N];
+			
+			for (int i = 0; i < M - 1; i++) {
+				for (int j = 1; j < N; j += 2) {
+					b[j] = true;	
+				}
+				for (int j = 2; j < N; j += 3) {
+					b[j] = !b[j];	
 				}
 			}
+
+			b[N-1] = !b[N-1]; // last iteration
+
+			int c = 0; // count
+			for (int i = 0; i < N; i++) {
+				c += b[i] ? 0 : 1;
+			}
+			
 			System.out.println(c);
 		}
 	}
